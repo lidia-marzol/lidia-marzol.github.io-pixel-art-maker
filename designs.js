a17e07f36f1bc7926c74cae677379001694d7956
Lidia <ldmarzol@gmail.com>
2018-01-29 15:30:31 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -5,8 +5,9 @@ var drawing = false;
   const $colorPicker = document.getElementById("colorPicker");
   const $sizePicker = document.getElementById("sizePicker");
   const $table = document.getElementById("pixel_canvas");
-  
+
   $sizePicker.addEventListener('submit', function() {
+
     event.preventDefault();
 
     let width = document.getElementById("input_width").value;
@@ -14,15 +15,15 @@ var drawing = false;
     makeGrid(width, height);
   })
   
-  
+
   function makeGrid(width, height) {
     $table.innerHTML = '';
     for (let row = 0; row < width; row++) {
       let newRow = $table.insertRow();
         for (let cell= 0; cell < height; cell++ ) {
           let newCell = newRow.insertCell();
-        
-       
+      
+    
           newCell.onmousedown = function() { drawing = true; this.style.background = $colorPicker.value; };
           newCell.onmouseup = function() { drawing = false; };
           newCell.onmouseover = changeColor;
